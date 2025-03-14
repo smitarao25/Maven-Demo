9357e08e819b081ab0d4d44ef04f8cfcc7a51865
Smita Rao <77341182+smitarao25@users.noreply.github.com>
2025-03-14 18:37:08 +0530

    Update App.java

--- src/main/java/com/gopi/sample/App.java
+++ src/main/java/com/gopi/sample/App.java
@@ -5,6 +5,6 @@ package com.gopi.sample;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello devops");
     }
 }
